fc21b13b14fc7ef65c18142ae2be55793acfaa8e
Mohamed idrissi <115729920+mohamed0009@users.noreply.github.com>
2024-11-08 17:55:40 +0100

    Initialisation project spring boot

---
+++ backend/backend/src/test/java/com/getclass/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getclass.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
